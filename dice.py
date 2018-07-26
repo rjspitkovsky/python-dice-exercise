45f49baaf9a9e38d70ed624046c0c7610390e6d5
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-07-26 14:52:20 -0400

    stubbing out no and error cases for user input

--- dice.py
+++ dice.py
@@ -1,1 +1,11 @@
-#The Goal: Like the title suggests, this project involves writing a program that simulates rolling dice. When the program runs, it will randomly choose a number between 1 and 6. (Or whatever other integer you prefer — the number of sides on the die is up to you.) The program will print what that number is. It should then ask you if you’d like to roll again.
+
+
+decision = raw_input("Would you like to roll the dice? Yes/No ")
+
+if decision == "No":
+    print("Thank you for playing.")
+    exit()
+
+if decision != "No" and decision != "Yes":
+    print("That answer was insufficient. Would you like to roll the dice? Yes/No")
+    decision = raw_input("Would you like to roll the dice? Yes/No ")
