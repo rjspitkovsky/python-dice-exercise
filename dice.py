ca80003398471b87aa552638f6edf2040bbf228d
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-07-26 15:28:51 -0400

    added instructions and comments

--- dice.py
+++ dice.py
@@ -1,9 +1,12 @@
+# The goal: Simulate rolling dice. The program will randomly choose a number between 1 and 6. The program will then print what that number is. Then ask the user if they'd like to roll again.
+
+
 def get_number():
     from random import randint
     print(randint(1, 6))
 
 
-
+# idea for refactor: use while loops instead of conditional logic? 
 
 def roll_dice():
     decision = raw_input("Would you like to roll the dice? yes/no ").strip()
